ea2fa16b78a8c2271ee74299ee20c8ba4bf849a5
MenaWANG <ningwang25@gmail.com>
2022-04-03 13:09:18 +1000

    Update Math's Loop Simulation.py
    Brilliant, just added some annotation and changed variable names to help (myself) understand. :)

--- Math's Loop Simulation.py
+++ Math's Loop Simulation.py
@@ -1,17 +1,17 @@
 import random
 from random import randint
 list = [1, 2, 3]
-truenumber = 0
-falsenumber = 0
-for i in range(3000):
+truetotal = 0 # to count the total number of sucesses
+falsetotal = 0 # to count the total number of failure
+for i in range(10000): 
     trueorfalse = []
-    random = randint(1, 3)
+    random = randint(1, 3) 
     for i in list:
         trueorfalse.append(i == random)
-    trueorfalse.pop()
-    trueorfalse.remove(False)
-    if trueorfalse[0] == True:
-        truenumber += 1
+    trueorfalse.pop() 
+    trueorfalse.remove(False) # the judge takes one false number away
+    if trueorfalse[0] == True: 
+        truetotal += 1
     else:
-        falsenumber += 1
-print(truenumber/(truenumber+falsenumber)*100)
+        falsetotal += 1
+print('The percentage of success is ', truetotal/(truetotal+falsetotal)*100)
