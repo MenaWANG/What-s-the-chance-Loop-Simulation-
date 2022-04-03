3f9111620545e02a35e44288945c4be4efe2f95f
MenaWANG <ningwang25@gmail.com>
2022-04-03 10:55:51 +1000

    Update Math's Loop Simulation.py
    This is brilliant! Just added some annotation and changed variable names to help (myself) understand. :)

--- Math's Loop Simulation.py
+++ Math's Loop Simulation.py
@@ -1,17 +1,17 @@
 import random
 from random import randint
 list = [1, 2, 3]
-truenumber = 0
-falsenumber = 0
-for i in range(3000):
-    trueorfalse = []
-    random = randint(1, 3)
+truetotal = 0 # count the times of picking the TRUE number
+falsetotal = 0 # count the times of picking the FALSE number
+for i in range(10000): # try the strategy 10,000 times
+    trueorfalse = [] 
+    random = randint(1, 3) # bring the randomness in: one in three is the TRUE number
     for i in list:
         trueorfalse.append(i == random)
-    trueorfalse.pop()
-    trueorfalse.remove(False)
-    if trueorfalse[0] == True:
-        truenumber += 1
-    else:
-        falsenumber += 1
-print(truenumber/(truenumber+falsenumber)*100)
+    trueorfalse.pop() # remove the number I picked
+    trueorfalse.remove(False) # the judge remove the FALSE number left
+    if trueorfalse[0] == True: # if the number left is TRUE, then one more success
+        truetotal += 1
+    else: # else one more failure
+        falsetotal += 1
+print('The chance of success if I always choose the number left by the judge: ', truetotal/(truetotal+falsetotal)*100)
